2cce108c28945a7f67ff81e535f162b6ce6aa9c3
Timur Abdullin <timur.122@yandex.ru>
2021-02-20 13:56:58 +0300

    Initial commit

---
+++ PomoikaToConvert/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PomoikaToConvert
+{
+    public class Startup
+    {
+        public void ConfigureServises(IServiceCollection service)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/",
+                    async context => { await context.Response.WriteAsync("Hi my name is George I am from Chilladelphia"); });
+            });
+        }
+    }
+}
